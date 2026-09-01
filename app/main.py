4ffbb2566dd6e8493d46baff593f9abd9ff21bd9
lilseniorj <jesusvarguer18@gmail.com>
2026-08-31 20:29:18 -0500

    Estructura tu FastAPI con APIRouter

---
+++ app/main.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import zoneinfo
+from datetime import datetime
+
+from fastapi import FastAPI
+
+from db import create_all_tables
+from models import Invoice, Transaction
+from .routers import customers as customer
+
+
+
+app = FastAPI(lifespan=create_all_tables)
+app.include_router(customer.router)
+
+
+@app.get("/")
+async def root():
+
+    return {"message": "Hello Lil Seniorj"}
+
+
+country_timezone = {
+    "CO": "America/Bogota",
+    "US": "America/New_York",
+    "MX": "America/Mexico_City",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
+
+
+@app.get("/time/{iso_code}")
+async def time(iso_code: str, format_24: bool = True):
+    iso = iso_code.upper()
+    timezone_str = country_timezone.get(iso, "UTC")
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    datetime_format = "%H:%M:%S" if format_24 else "%I:%M:%S %p"
+    return {
+        "iso_code": iso,
+        "timezone": timezone_str,
+        "time": datetime.now(tz).strftime(datetime_format),
+    }
+
+
+@app.post("/transactions")
+async def create_transaction(transaction_data: Transaction):
+    return transaction_data
+
+
+@app.post("/invoices")
+async def create_invoice(invoice_data: Invoice):
+    return invoice_data
--- app/routers/customers.py
+++ app/routers/customers.py
@@ -1,49 +1,16 @@
-from __future__ import annotations
-
-import zoneinfo
-from datetime import datetime
-
-from fastapi import FastAPI, HTTPException
+from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
-from db import SessionDependency, create_all_tables
-from models import Customer, CustomerCreate, CustomerUpdate, Invoice, Transaction
+from db import SessionDependency
+from models import Customer, CustomerCreate, CustomerUpdate
 
-app = FastAPI(lifespan=create_all_tables)
-
-
-@app.get("/")
-async def root():
-
-    return {"message": "Hello Lil Seniorj"}
-
-
-country_timezone = {
-    "CO": "America/Bogota",
-    "US": "America/New_York",
-    "MX": "America/Mexico_City",
-    "BR": "America/Sao_Paulo",
-    "PE": "America/Lima",
-}
-
-
-@app.get("/time/{iso_code}")
-async def time(iso_code: str, format_24: bool = True):
-    iso = iso_code.upper()
-    timezone_str = country_timezone.get(iso, "UTC")
-    tz = zoneinfo.ZoneInfo(timezone_str)
-    datetime_format = "%H:%M:%S" if format_24 else "%I:%M:%S %p"
-    return {
-        "iso_code": iso,
-        "timezone": timezone_str,
-        "time": datetime.now(tz).strftime(datetime_format),
-    }
+router = APIRouter()
 
 
 db_customers: list[Customer] = []
 
 
-@app.post("/customers", response_model=Customer)
+@router.post("/customers", response_model=Customer, tags=["customers"])
 async def create_customer(customer_data: CustomerCreate, session: SessionDependency):
     customer = Customer.model_validate(customer_data.model_dump())
     session.add(customer)
@@ -52,7 +19,7 @@ async def create_customer(customer_data: CustomerCreate, session: SessionDepende
     return customer
 
 
-@app.get("/customers/{customer_id}", response_model=Customer)
+@router.get("/customers/{customer_id}", response_model=Customer, tags=["customers"])
 async def read_customer(customer_id: int, session: SessionDependency):
     customer_db = session.get(Customer, customer_id)
     if not customer_db:
@@ -60,8 +27,10 @@ async def read_customer(customer_id: int, session: SessionDependency):
     return customer_db
 
 
-@app.patch("/customers/{customer_id}", response_model=Customer)
-async def update_customer(customer_id: int, customer_data: CustomerUpdate, session: SessionDependency):
+@router.patch("/customers/{customer_id}", response_model=Customer,tags=["customers"])
+async def update_customer(
+    customer_id: int, customer_data: CustomerUpdate, session: SessionDependency
+):
     # 1. Buscar si el cliente existe en la base de datos
     customer_db = session.get(Customer, customer_id)
     if not customer_db:
@@ -81,7 +50,7 @@ async def update_customer(customer_id: int, customer_data: CustomerUpdate, sessi
     return customer_db
 
 
-@app.delete("/customers/{customer_id}")
+@router.delete("/customers/{customer_id}", tags=["customers"])
 async def delete_customer(customer_id: int, session: SessionDependency):
     customer_db = session.get(Customer, customer_id)
     if not customer_db:
@@ -91,24 +60,14 @@ async def delete_customer(customer_id: int, session: SessionDependency):
     return {"detail": "Ok, customer deleted successfully"}
 
 
-@app.get("/customers", response_model=list[Customer])
+@router.get("/customers", response_model=list[Customer], tags=["customers"])
 async def list_customer(session: SessionDependency):
     return session.exec(select(Customer)).all()
 
 
-@app.get("/customers/{id}", response_model=Customer)
+@router.get("/customers/{id}", response_model=Customer, tags=["customers"])
 async def get_customer(id: int):
     for customer in db_customers:
         if customer.id == id:
             return customer
     raise HTTPException(status_code=404, detail="Customer no encontrado")
-
-
-@app.post("/transactions")
-async def create_transaction(transaction_data: Transaction):
-    return transaction_data
-
-
-@app.post("/invoices")
-async def create_invoice(invoice_data: Invoice):
-    return invoice_data
